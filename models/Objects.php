96eae0cbe40e49ddaf7c1ee8fbf0c19c50e543de
NielsvandenDries <niels@openmindedit.nl>
2023-03-27 13:37:27 +0200

    rollback

--- models/Objects.php
+++ models/Objects.php
@@ -9,20 +9,11 @@ class Objects extends Model
 {
     use \October\Rain\Database\Traits\Validation;
     use \October\Rain\Database\Traits\SoftDelete;
-    use \October\Rain\Database\Traits\Sluggable;
 
-    protected $slugs = ['slug' => 'id'];
     protected $dates = ['deleted_at'];
-    protected $slugging = ['forceUpdate' => true];
-
-    /**
-     * @var string The database table used by the model.
-     */
+    
     public $table = 'openmindedit_fieldengineeringtoolkit_objects';
 
-    /**
-     * @var array Validation rules
-     */
     public $rules = [
     ];
 
@@ -41,34 +32,4 @@ class Objects extends Model
     public $attachMany = [
         'photos' => 'System\Models\File'
     ];
-
-    public function getSluggableAttributes()
-    {
-        return $this->slugs;
-    }
-
-    public function makeSlugUnique($slug, $field, $value, $config)
-    {
-        $count = 2;
-        $originalSlug = $slug;
-
-        // Check if a record with the same slug already exists
-        while ($this->newQuery()
-                   ->where($field, $slug)
-                   ->where('id', '<>', $this->id ?? null)
-                   ->first()) {
-            $slug = $originalSlug . '-' . $count++;
-        }
-
-        return $slug;
-    }
-
-    public static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($model) {
-            $model->slug = $model->generateSlug($model->id);
-        });
-    }
 }
